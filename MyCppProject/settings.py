3ce619d8381b1924c7fe6adf979ca8bc4ee54f00
EC2 Default User <ec2-user@ip-172-31-71-211.ec2.internal>
2023-07-27 20:10:13 +0000

    Change settings

--- MyCppProject/settings.py
+++ MyCppProject/settings.py
@@ -27,7 +27,7 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
