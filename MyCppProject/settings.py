4c0f04bd0101c980f32d9bdfa9122aed5f9e9117
EC2 Default User <ec2-user@ip-172-31-34-108.ec2.internal>
2023-07-28 17:34:00 +0000

    Update Settings

--- MyCppProject/settings.py
+++ MyCppProject/settings.py
@@ -27,7 +27,7 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'MyCppProject-env.eba-g9ppwc2x.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
