c1a4cd157885004b01ae0f375153d3956a42c785
EC2 Default User <ec2-user@ip-172-31-75-161.ec2.internal>
2023-07-31 18:07:40 +0000

    Add search functionality

--- MyShopApp/urls.py
+++ MyShopApp/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("products/<int:myid>", views.productView, name="ProductView"),
     path("checkout/", views.checkoutView, name="checkoutView"),
-    path("acknowledgement/", views.acknowledgementView, name="acknowledgement")
+    path("acknowledgement/", views.acknowledgementView, name="acknowledgement"),
+    path('search/', views.search_results, name='search_results'),
 ]
--- MyShopApp/views.py
+++ MyShopApp/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect, render
 from .models import Product
-from math import ceil
-
+from django.db.models import Q
 
 def index(request):
     products = Product.objects.all()
@@ -13,6 +12,21 @@ def index(request):
     params = {'products': products, 'cart_length': cart_length}
 
     return render(request, "MyShopApp/index.html", params)
+
+def search_results(request):
+    query = request.GET.get('q')
+    if query:
+        products = Product.objects.filter(
+            Q(product_name__icontains=query) | Q(desc__icontains=query)
+        )
+    else:
+        products = Product.objects.none()
+
+    cart = request.session.get('cart', {})
+    cart_length = sum(cart.values())
+
+    params = {'products': products, 'cart_length': cart_length, 'query': query}
+    return render(request, "MyShopApp/search_results.html", params)
 
 
 def productView(request, myid):
